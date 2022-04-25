ac949a50f88add999d1ac38c9972a524f80e236f
Nigaba <46766859+Nigaba@users.noreply.github.com>
2022-04-24 23:17:53 -0500

    Add files via upload
    upload version includes react webpage, web API, Mysql database, and adminer database manager

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# final-projj
+# final-project
+
+# Getting Started
+- `docker compose up`
+- **Web UI Endpoint**: `localhost:8081`
+- **API Endpoint**: `localhost:8082`
+- **DB Manager Endpoint**: `localhost:8084`
--- src/app/next.config.js
+++ src/app/next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    outputStandalone: true,
-  },
+	reactStrictMode: true,
+	experimental: {
+		outputStandalone: true,
+	},
+	webpackDevMiddleware: config => {
+		config.watchOptions = {
+			poll: 1000,
+			aggregateTimeout: 300
+		}
+		return config;
+	},
 }
 
-module.exports = nextConfig
+export default nextConfig;
